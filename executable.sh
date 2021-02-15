0f42682e14dfa8919747e528892254c49f4a0ff7
kostjaigin <kostjaigin@gmail.com>
2021-02-15 19:14:11 +0100

    change build version in exe.sh

--- executable.sh
+++ executable.sh
@@ -1,10 +1,10 @@
 $SPARK_HOME/bin/spark-submit \
 --master k8s://https://130.149.249.40:6443 \
 --deploy-mode cluster \
---conf spark.executor.instances=$2 \
+--conf spark.executor.instances=5 \
 --conf spark.kubernetes.namespace=konstantin \
 --conf spark.kubernetes.authenticate.driver.serviceAccountName=konstantin \
---conf spark.kubernetes.container.image=docker.io/kostjaigin/spark-py:v3.0.1-Ugin_0.0.7 \
+--conf spark.kubernetes.container.image=docker.io/kostjaigin/spark-py:v3.0.1-Ugin_0.0.6 \
 --files "local:///opt/spark/data/build/dll/libgnn.d" \
 --files "local:///opt/spark/data/build/dll/libgnn.so" \
 --files "local:///opt/spark/data/build/lib/config.d" \
@@ -15,15 +15,5 @@ $SPARK_HOME/bin/spark-submit \
 --files "local:///opt/spark/data/build/lib/msg_pass.o" \
 --files "local:///opt/spark/data/models/USAir_hyper.pkl" \
 --files "local:///opt/spark/data/models/USAir_model.pth" \
---files "local:///opt/spark/data/prediction_data/USAir.mat" \
---files "local:///opt/spark/data/models/PB_hyper.pkl" \
---files "local:///opt/spark/data/models/PB_model.pth" \
---files "local:///opt/spark/data/prediction_data/PB.mat" \
---files "local:///opt/spark/data/models/facebook_hyper.pkl" \
---files "local:///opt/spark/data/models/facebook_model.pth" \
---files "local:///opt/spark/data/prediction_data/facebook.mat" \
---files "local:///opt/spark/data/models/arxiv_hyper.pkl" \
---files "local:///opt/spark/data/models/arxiv_model.pth" \
---files "local:///opt/spark/data/prediction_data/arxiv.mat" \
 --py-files "local:///opt/spark/data/dependencies.zip" \
-local:///opt/spark/data/App.py $@
+local:///opt/spark/data/App.py
--- execute.sh
+++ execute.sh
@@ -4,7 +4,7 @@ $SPARK_HOME/bin/spark-submit \
 --conf spark.executor.instances=5 \
 --conf spark.kubernetes.namespace=konstantin \
 --conf spark.kubernetes.authenticate.driver.serviceAccountName=konstantin \
---conf spark.kubernetes.container.image=docker.io/kostjaigin/spark-py:v3.0.1-Ugin_0.0.1 \
+--conf spark.kubernetes.container.image=docker.io/kostjaigin/spark-py:v3.0.1-Ugin_0.0.6 \
 --files "local:///opt/spark/data/models/USAir_hyper.pkl","local:///opt/spark/data/models/USAir_model.pth","local:///opt/spark/data/build/dll/libgnn.d","local:///opt/spark/data/build/dll/libgnn.so","local:///opt/spark/data/build/lib/config.d","local:///opt/spark/data/build/lib/config.o","local:///opt/spark/data/build/lib/graph_struct.d","local:///opt/spark/data/build/lib/graph_struct.o","local:///opt/spark/data/build/lib/msg_pass.d","local:///opt/spark/data/build/lib/msg_pass.o" \
 --py-files "local:///opt/spark/data/dependencies.zip" \
 local:///opt/spark/data/App.py
