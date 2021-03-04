14b1706e6f17390100ec68f53715f2c00d031bca
Konstantin Igin <konstantin.igin@wally030.cit.tu-berlin.de>
2021-03-04 18:59:13 +0100

    extended experiments script

--- experiments.sh
+++ experiments.sh
@@ -1,16 +1,36 @@
-export SPARK_HOME='/home/konstantin.igin/bachelor';
 for hop in 1 2; do
-	for dataset in "USAir" "yeast" "PB" "facebook" "arxiv"; do
-		$SPARK_HOME/load_db.sh $dataset;
-		# for db in "True" "False"; do
-		# for executors in 4 8 12; do
+	for dataset in "USAir" "yeast" "PB"; do
+		#$SPARK_HOME/load_db.sh $dataset;
+		#for db in "True" "False"; do
+		for executors in 4 8 12; do
 			for links in 10 100 500 1000; do
 				echo "|EXPERIMENT SETUP| " "hop: " $hop "|dataset: " $dataset "|# links: " $links "|Batch in prior? " $batch_inprior "|# of execs: " $executors " |";
-				$SPARK_HOME/exe.sh --number_of_executors 4 --hop $hop --links $links --db_extraction "True" --dataset $dataset;
+				$SPARK_HOME/exe.sh --number_of_executors $executors --hop $hop --links $links --db_extraction "False" --dataset $dataset --hdfs_host "130.149.249.25";
 				# clean up
 				kubectl get pods --field-selector 'status.phase=Succeeded' -o name | xargs kubectl delete;
 			done
 		#done
-		# done
+		done
+	done
+done
+
+for hop in 1 2; do
+	for dataset in "facebook" "arxiv"; do
+		for executors in 4 8 12; do
+			for links in 10 100 500; do
+				$SPARK_HOME/exe.sh --number_of_executors $executors --hop $hop --links $links --db_extraction "False" --dataset $dataset --hdfs_host "130.149.249.25";
+				# clean up
+                                kubectl get pods --field-selector 'status.phase=Succeeded' -o name | xargs kubectl delete;
+			done
+		done
+	done
+done
+
+# 1000 links hop 1
+for dataset in "facebook" "arxiv"; do
+	for executors in 12 8 4; do
+		$SPARK_HOME/exe.sh --number_of_executors $executors --hop 1 --links 1000 --db_extraction "False" --dataset $dataset --hdfs_host "130.149.249.25";
+        	# clean up
+        	kubectl get pods --field-selector 'status.phase=Succeeded' -o name | xargs kubectl delete;
 	done
 done
